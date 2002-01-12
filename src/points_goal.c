8eb1c5ceeecc757c1fa6e5de8a124e1b547e43a9
grelminar <grelminar@yahoo.com>
2002-01-11 17:37:35 -0800

    added new points_goal file (and added to be loaded by zone) additions to out-of-process.txt

---
+++ src/points_goal.c
@@ -0,0 +1,130 @@
+
+#include "asss.h"
+
+#include "settings/soccer.h"
+
+
+#define MODULE "points_goal"
+
+struct ArenaScores
+{
+	int score[2];
+};
+
+/* prototypes */
+
+local void MyGoal(int, int, int, int, int);
+local void MyAA(int, int);
+
+/* global data */
+
+local struct ArenaScores scores[MAXARENA];
+
+local Imodman *mm;
+local Iplayerdata *pd;
+local Iballs *balls;
+local Iarenaman *aman;
+local Iconfig *cfg;
+local Ichat *chat;
+
+
+int MM_points_goal(int action, Imodman *mm_, int arena)
+{
+	if (action == MM_LOAD)
+	{
+		mm = mm_;
+		mm->RegInterest(I_PLAYERDATA, &pd);
+		mm->RegInterest(I_BALLS, &balls);
+		mm->RegInterest(I_ARENAMAN, &aman);
+		mm->RegInterest(I_CONFIG, &cfg);
+		mm->RegInterest(I_CHAT, &chat);
+		return MM_OK;
+	}
+	else if (action == MM_UNLOAD)
+	{
+		mm->UnregInterest(I_CHAT, &chat);
+		mm->UnregInterest(I_CONFIG, &cfg);
+		mm->UnregInterest(I_ARENAMAN, &aman);
+		mm->UnregInterest(I_BALLS, &balls);
+		mm->UnregInterest(I_PLAYERDATA, &pd);
+		return MM_OK;
+	}
+	else if (action == MM_ATTACH)
+	{
+		mm->RegCallback(CB_ARENAACTION, MyAA, arena);
+		mm->RegCallback(CB_GOAL, MyGoal, arena);
+		return MM_OK;
+	}
+	else if (action == MM_DETACH)
+	{
+		mm->UnregCallback(CB_GOAL, MyGoal, arena);
+		mm->UnregCallback(CB_ARENAACTION, MyAA, arena);
+		return MM_OK;
+	}
+	else if (action == MM_CHECKBUILD)
+		return BUILDNUMBER;
+	return MM_FAIL;
+}
+
+
+void MyAA(int arena, int action)
+{
+	if (action == AA_CREATE)
+	{
+		int i;
+		for (i = 0; i < 2; i++)
+			scores[arena].score[i] = 0;
+	}
+}
+
+
+void MyGoal(int arena, int pid, int bid, int x, int y)
+{
+	int mode, freq = 0, i;
+	int teamset[MAXPLAYERS], nmeset[MAXPLAYERS];
+	int teamc = 0, nmec = 0;
+
+	mode = cfg->GetInt(aman->arenas[arena].cfg, "Soccer", "Mode", 0);
+
+	switch (mode)
+	{
+		case GOAL_ALL:
+			freq = pd->players[pid].freq;
+			break;
+
+		case GOAL_LEFTRIGHT:
+			freq = x < 512 ? 1 : 0;
+			break;
+
+		case GOAL_TOPBOTTOM:
+			freq = y < 512 ? 1 : 0;
+			break;
+
+		case GOAL_QUADRENTS_3_1:
+		case GOAL_QUADRENTS_1_3:
+		case GOAL_WEDGES_3_1:
+		case GOAL_WEDGES_1_3:
+			/* not implemented */
+			break;
+	}
+
+	scores[arena].score[freq]++;
+
+	pd->LockStatus();
+	for (i = 0; i < MAXPLAYERS; i++)
+		if (pd->players[i].status == S_PLAYING &&
+		    pd->players[i].arena == arena)
+		{
+			if (pd->players[i].freq == freq)
+				teamset[teamc++] = i;
+			else
+				nmeset[nmec++] = i;
+		}
+	pd->UnlockStatus();
+
+	teamset[teamc] = nmeset[nmec] = -1;
+	chat->SendSetMessage(teamset, "Team Goal! by %s  Reward:1", pd->players[pid].name);
+	chat->SendSetMessage(nmeset, "Enemy Goal! by %s  Reward:1", pd->players[pid].name);
+}
+
+
