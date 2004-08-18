8ca34a8a2f6b19eb451dcc44f1a7d0ce34ba8e97
grelminar <grelminar@yahoo.com>
2004-08-18 13:51:06 -0700

    added a check for ship type in SetShipAndFreq.

--- src/game.c
+++ src/game.c
@@ -566,7 +566,7 @@ local void SetFreqAndShip(Player *p, int ship, int freq)
 		return;
 	}
 
-	if (freq < 0 || freq > 9999)
+	if (freq < 0 || freq > 9999 || ship < 0 || ship > SPEC)
 		return;
 
 	pd->LockPlayer(p);
