fb4340d53caa8c9fa3e64cad3c0e336be54c5856
drbrain <drbrain@gmail.com>
2009-10-04 10:59:46 -0400

    Added Misc:NoSafeAntiwarp for bug #60.

--- src/core/game.c
+++ src/core/game.c
@@ -52,6 +52,7 @@ typedef struct
 	int initlockship, initspec;
 	int deathwofiring;
 	int regionchecktime;
+	int nosafeanti;
 } adata;
 
 
@@ -398,6 +399,14 @@ local void handle_ppk(Player *p, struct C2SPosition *pos, int len, int isfake)
 		     pos->weapon.alternate)
 			sendtoall = TRUE;
 
+		/* disable antiwarp if they're in a safe and nosafeanti is on */
+		if ((pos->status & STATUS_ANTIWARP) &&
+		     (pos->status & STATUS_SAFEZONE) &&
+		     adata->nosafeanti)
+		{
+			pos->status &= ~STATUS_ANTIWARP;
+		}
+
 		/* send some percent of antiwarp positions to everyone */
 		if ( pos->weapon.type == 0 &&
 		     (pos->status & STATUS_ANTIWARP) &&
@@ -598,6 +607,7 @@ local void FakePosition(Player *p, struct C2SPosition *pos, int len)
 local int IsAntiwarped(Player *p, LinkedList *players)
 {
 	pdata *data = PPDATA(p, pdkey);
+	adata *adata = P_ARENA_DATA(p->arena, adkey);
 	pdata *idata;
 	Player *i;
 	Link *link;
@@ -608,8 +618,9 @@ local int IsAntiwarped(Player *p, LinkedList *players)
 		pd->Lock();
 		FOR_EACH_PLAYER_P(i, idata, pdkey)
 		{
-			if(i->arena == p->arena && i->p_freq != p->p_freq && i->p_ship != SHIP_SPEC
-					&& (i->position.status & STATUS_ANTIWARP) && !idata->rgnnoanti)
+			if (i->arena == p->arena && i->p_freq != p->p_freq && i->p_ship != SHIP_SPEC
+					&& (i->position.status & STATUS_ANTIWARP) && !idata->rgnnoanti
+					&& (!(i->position.status & STATUS_SAFEZONE) || !adata->nosafeanti))
 			{
 				int xdelta = (i->position.x - p->position.x);
 				int ydelta = (i->position.y - p->position.y);
@@ -1453,6 +1464,11 @@ local void ArenaAction(Arena *arena, int action)
 		 * before being placed in spectator mode. */
 		ad->deathwofiring =
 			cfg->GetInt(arena->cfg, "Security", "MaxDeathWithoutFiring", 5);
+
+		/* cfghelp: Misc:NoSafeAntiwarp, arena, int, def: 0
+		 * Disables antiwarp on players in safe zones. */
+		ad->nosafeanti = 
+			cfg->GetInt(arena->cfg, "Misc", "NoSafeAntiwarp", 0);
 
 		/* cfghelp: Prize:DontShareThor, arena, bool, def: 0
 		 * Whether Thor greens don't go to the whole team. */
