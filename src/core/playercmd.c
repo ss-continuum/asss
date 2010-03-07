47d65f35dcdb9135cec5f5c78f1d4de884c207aa
drbrain <drbrain@gmail.com>
2010-03-07 11:55:58 -0500

    Renamed ?recycle to ?recyclezone to prevent bad typos on ?recyclearena. This fixes bug #84.

--- src/core/playercmd.c
+++ src/core/playercmd.c
@@ -278,14 +278,14 @@ local void Cshutdown(const char *tc, const char *params, Player *p, const Target
 	ml->Quit(code);
 }
 
-local helptext_t recycle_help =
+local helptext_t recyclezone_help =
 "Targets: none\n"
 "Args: none\n"
 "Immediately shuts down the server, exiting with {EXIT_RECYCLE}. The "
 "{run-asss} script, if it is being used, will notice {EXIT_RECYCLE} "
 "and restart the server.\n";
 
-local void Crecycle(const char *tc, const char *params, Player *p, const Target *target)
+local void Crecyclezone(const char *tc, const char *params, Player *p, const Target *target)
 {
 	ml->Quit(EXIT_RECYCLE);
 }
@@ -2732,7 +2732,7 @@ local const struct cmd_info core_commands[] =
 	CMD(disablecmdgroup)
 	CMD(arena)
 	CMD(shutdown)
-	CMD(recycle)
+	CMD(recyclezone)
 	CMD(owner)
 	CMD(zone)
 	CMD(version)
