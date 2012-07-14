b09859cbbf8a25bd6c1f818736f35647df08bb25
numpf <numpf.x@gmail.com>
2012-07-14 11:44:22 -0400

    python comments for clientset.h

--- src/include/clientset.h
+++ src/include/clientset.h
@@ -19,25 +19,41 @@ typedef u32 override_key_t;
 typedef struct Iclientset
 {
 	INTERFACE_HEAD_DECL
+	/* pyint: use */
 
 	void (*SendClientSettings)(Player *p);
+	/* pyint: player -> void */
 
 	u32 (*GetChecksum)(Player *p, u32 key);
 
 	int (*GetRandomPrize)(Arena *arena);
+	/* pyint: arena -> int */
 
 	override_key_t (*GetOverrideKey)(const char *section, const char *key);
+	/* pyint: string, string -> int */
 	/* zero return means failure */
 
 	void (*ArenaOverride)(Arena *arena, override_key_t key, i32 val);
+	/* pyint: arena, int, int -> void */
 	void (*ArenaUnoverride)(Arena *arena, override_key_t key);
+	/* pyint: arena, int -> void */
+	
+	/* The return value for these 2 is a boolean, 0 = failure */
 	int (*GetArenaOverride)(Arena *arena, override_key_t key, int *value);
+	/* pyint: arena, int, int out -> int */
 	int (*GetArenaValue)(Arena *arena, override_key_t key);
+	/* pyint: arena, int -> int */
 
 	void (*PlayerOverride)(Player *p, override_key_t key, i32 val);
+	/* pyint: player, int, int -> void */
 	void (*PlayerUnoverride)(Player *p, override_key_t key);
+	/* pyint: player, int -> void */
+
+	/* The return value for these 2 is a boolean, 0 = failure */
 	int (*GetPlayerOverride)(Player *p, override_key_t key, int *value);
+	/* pyint: player, int, int out -> int */
 	int (*GetPlayerValue)(Player *p, override_key_t key);
+	/* pyint: player, int -> int */
 } Iclientset;
 
 
