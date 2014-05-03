eb4320a1c1f5538c48583aceea3b79fd7fd0c8fa
JoWie <jowie@welcome-to-the-machine.com>
2014-05-03 17:24:04 +0200

    bump version number :)
    --HG--
    branch : jowie

--- src/include/defs.h
+++ src/include/defs.h
@@ -26,7 +26,7 @@
 #endif
 
 /** a version number, represented as a string */
-#define ASSSVERSION "1.5.0"
+#define ASSSVERSION "1.5.1"
 /** a version number, represented as an integer */
 #define ASSSVERSION_NUM 0x00010500
 #define BUILDDATE __DATE__ " " __TIME__
